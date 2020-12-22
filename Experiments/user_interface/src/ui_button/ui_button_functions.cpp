c9b628b4ce1c2ae4c7e7a815b00f85adaf815ae0
Haley Read <haleyread30@gmail.com>
2020-12-22 12:22:51 -0800

    First attempt at timer migration
    Apollo3 does not support these timer registers

--- Experiments/user_interface/src/ui_button/ui_button_functions.cpp
+++ Experiments/user_interface/src/ui_button/ui_button_functions.cpp
@@ -2,21 +2,23 @@
 #include "ui_button_class.h"
 
 // Local Variables
-char buttonPin = PD1;              // TO DO: Move Pin Assignment To Global Pin Assignment List
-int ledPin = 13;
-int select = 0;
-int next   = 0;
-int previousState = 1;       // Used to determine button transitions
-unsigned int previousPress;     // Used to time button press durations
-int buttonDebounce  = 20;       // Used to debounce button input (20ms)
-int selectTimer     = 500;      // Used to identify Select vs Next (500ms)
+char buttonPin                  = RX1;  // TO DO: Move Pin Assignment To Global Pin Assignment List
+int ledPin                      = 13;
+int buttonSelect                      = 0;
+int selectDetect                = 0;
+int next                        = 0;
+int currentButtonState          = 1;
+int lastButtonState             = 1;    // Used to determine button transitions
+unsigned long lastDebounceTime  = 0;    // Used to time button press durations
+int minDebounceTime             = 20;   // Used to debounce button input (20ms)
+int selectTimer                 = 500;  // Used to identify Select vs Next (500ms)
 
 
 // Class Functions
 int ui_button_class::checkSelect()
 {
-    int temp = select;
-    select = 0;
+    int temp = buttonSelect;
+    buttonSelect = 0;
     return temp;
     
 }
@@ -25,57 +27,103 @@ int ui_button_class::checkNext()
 {
     int temp = next;
     next = 0;
-    select = 0;
+    buttonSelect = 0;
     return temp;
 }
 
 void ui_button_class::ui_button_setup()
 {
-  PCMSK2 |= bit (PCINT17); 	// Pin TXO
-  PCIFR	 |= bit (PCIF2);	// Clear any outstanding interrupts
-  PCICR  |= bit (PCIE2);	// Enable pin change interrupts for PCINT23 to PCINT16
+    pinMode(buttonPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    
+    // Set Initial LED State
+    digitalWrite(ledPin, OUTPUT);
 
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
+
+    // ---------------------------------------------
+    // MOVE LINES BELOW TO TOP-LEVEL INTERRUPT SETUP
+    // ---------------------------------------------
+    cli();          // stop interrupts
+    
+    // Set timer0 interrupt at 1kHz
+    TCCR0A  = 0;    // set entire TCCR0A register to 0
+    TCCR0B  = 0;    // same for TCCR0B
+    
+    // Set compare match register for 2kHz increments
+    OCR0A   = 249;  // = (16*10^6) / (1000*64) - (must be <256)
+    
+    // Turn on CTC mode
+    TCCR0A  |= (1 << WGM01);
+    
+    // Set CS01 and CS00 bits for 64 prescaler
+    TCCR0B  |= (1 << CS01) | (1 << CS00);
+    
+    // Enable timer compare interrupt
+    TIMSK0  |= (1 << OCIE0A);
+
+    sei();          // Allow interrupts
 }
 
 
 // Global Functions
-void buttonEvent()
+void buttonCheck()
 {
-  ui_button_class ui_btn;
-  // Button Debouncer
-  if ((millis() - previousPress) > buttonDebounce)
-  {
-    // Button Pushed
-    if (digitalRead(buttonPin) == LOW && previousState == HIGH)
+    // Read button value into local varaiable
+    int buttonValue = digitalRead(buttonPin);
+
+    // If the button changed (due to noise or pressing)
+    if (buttonValue != lastButtonState)
     {
-      previousPress = millis();
-      previousState = LOW;
+        // Reset debounce timer
+        lastDebounceTime = millis();
     }
-
-    // Button Released
-    else if (digitalRead(buttonPin) == HIGH && previousState == LOW)
+    
+    // Only check button state after debounce timer elapsed
+    if ((millis() - lastDebounceTime) > minDebounceTime)
     {
+        // If button state changed:
+        if (buttonValue != currentButtonState)
+        {
+            currentButtonState = buttonValue;
+            
+            // Button Pushed, do nothing
+            //if (buttonState == LOW)
+            //{
+            //    
+            //}
+            
+            // Button Released
+            if (buttonState == HIGH)
+            {
+                // Check for select detect
+                if (selectDetect == HIGH)
+                {
+                    selectDetect    = 0;
+                }
+                // Otherwise next was desired
+                else
+                {
+                    next            = 1;
+                }
+            }
+        }
 
-      // Select
-      if ((millis() - previousPress) > selectTimer)
-      {
-        //oled.main_menu();
-        select = 1;
-      }
-
-      // Next
-      else
-      {
-        next = 1;
-      }
-      previousState = HIGH;
+        // Check for button being held down once per press
+        if (buttonState == LOW && selectDetect == 0)
+        {
+            // Check Select timer
+            if ((millis() - lastDebounceTime) > selectTimer)
+            {
+                buttonSelect          = 1;
+                selectDetect    = 1;
+            }
+        }
     }
-  }
 }
 
-ISR(PCINT2_vect)
+
+// Timer0 interrupt 1kHz calls buttonCheck
+ISR(TIMER0_COMPA_vect)
 {
-  buttonEvent();
+  buttonCheck();
 }
